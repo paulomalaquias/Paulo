e24f5db81c3c78e5ee9335cbf67806cd98f1f05c
Paulo Nataniel <paulomalaquias23@gmail.com>
2022-06-10 21:29:40 -0300

    aula dia 10.06

---
+++ contatos.php
@@ -0,0 +1,25 @@
+<?php
+	//echo $_POST["usuario"];
+	//echo "<br />";
+	//echo $_POST["email"];
+	//echo "<br />";
+	//echo $_POST["tipo-contato"];
+	
+	//echo $_POST["numero1"];
+	//echo "<br />";
+	//$numero1 = $_GET["numero1"];
+	//$numero2 = $_GET["numero2"];
+	//$resultado = $numero1 + $numero2;
+	//echo $resultado;
+	//var_dump($_GET);
+	//echo "<br />";
+
+
+	$nome = $_GET["usuario"];
+	$email = $_GET["email"];
+	$tipo_contato = $_GET["tipo-contato"];
+	echo $nome;
+	echo "<br />";
+	echo $email;
+	echo "<br />";
+	echo $tipo_contato;
